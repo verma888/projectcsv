86a52e0c3d0b814ba68a17510dc842513c99e940
verma888 <51471939+verma888@users.noreply.github.com>
2019-08-03 13:25:42 +0530

    Add files via upload

---
+++ copyofapp.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonexport = require('jsonexport');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+MongoClient.connect(url,{ useNewUrlParser: true },function(err, client){
+    console.log("successfully connected to server");
+   
+
+    var app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+        app.listen(3000, () => {
+        console.log('Server is running on PORT3000');
+    });
+
+    app.post('/adduser', function(req, res){
+        console.log(req.body);
+        var rbody = req.body;
+        rbody.status = "N";
+        let dbName = "project385";
+        let db = client.db(dbName);
+        let collection = db.collection('reports');
+            collection.insertMany([req.body], function(err, result) {
+            console.log("data insertion has been done to collection");
+            res.send(result);    
+        });
+    });
+
+
+
+    app.get('/getstatus', (req, res, next)=>{
+        console.log(req.query);
+        let dbName = "project385";
+        let db = client.db(dbName);
+        let collection = db.collection('reports');
+        collection.findOne((req.query),function(err, docs){    
+            console.log("search done");
+           console.log(docs);
+    
+                let task = docs.Sdate;
+                let task2 = docs.Edate;
+                let task3 = docs.SC.catagory;
+                console.log("task is = " + task3);
+            
+                
+    
+                db.collection('transactions').find({category: 902 , date: { $in: [ task, task2 ]}}, {projection: {_id:0}}).toArray (function(err, scresult){
+                    console.log("search done");
+                    console.log(scresult);
+                    
+                       
+                       
+        
+                        jsonexport(scresult,function(err, csv){
+                            if(err) return console.log(err);
+                            console.log(csv);
+                            fs.writeFile('Resultcsv460.csv',csv,(err,csvres) => {
+                                if (err) {
+                                    return;
+                                } 
+                                //Status Update
+                                    let collection = db.collection('reports');
+                                        collection.updateOne({"SC.catagory":'809'}, {$set: { status:'Y'}}, function(err, result) {
+                                        console.log("task completed");
+                                        res.send(result);    
+                                });
+                                res.send(scresult);
+                            })
+                        })
+                })        
+            
+        
+        });
+    });
+});
